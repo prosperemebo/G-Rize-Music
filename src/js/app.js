a554681549ab8eca11ba8fe4eb1398c902642acb
Prosper Emebo <emeboprosper@gmail.com>
2022-03-26 03:18:13 +0100

    Added About Section

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+let swiper = new Swiper('.swiper', {
+  slidesPerView: 1.2,
+  spaceBetween: 20,
+  freeMode: true,
+  breakpoints: {
+    600: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+  // breakpoints: {
+  //   600: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //   },
+  //   900: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 40,
+  //   },
+  // },
+});
